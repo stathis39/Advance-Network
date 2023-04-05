48414bfabde4860dfb9e2004519a776e0cf23ea4
Ryunosuke <51161382+stathis39@users.noreply.github.com>
2023-04-05 11:13:10 +0300

    Add files via upload
    First db

---
+++ dump.sql
@@ -0,0 +1,32 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: PR; Type: TABLE DATA; Schema: public; Owner: rkwxbbmy_postgress
+--
+
+COPY "PR" (id, name, surname, email, phone) FROM stdin;
+\.
+
+
+--
+-- Data for Name: tables; Type: TABLE DATA; Schema: public; Owner: rkwxbbmy_postgress
+--
+
+COPY tables (id, "timestamp", pr, status) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
